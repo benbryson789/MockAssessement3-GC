83ba3d905339e54f66ac71b4ff4536d36c1d3a6b
benbryson789 <deborashelby@gmail.com>
2020-12-31 21:29:11 -0500

    initial commit

---
+++ MockAssessement3/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MockAssessement3.Models;
+
+namespace MockAssessement3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+
+        {
+            return View("GetAge");
+        }
+
+        public IActionResult Result(string userName, int age)
+        {
+            if (age >= 18)
+            {
+                ViewData["CanVote"] = $"{ userName} can vote";
+            }
+            if (age >= 21)
+            {
+                ViewData["CanDrink"] = $"{userName} can Drink";
+            }
+            if (age < 18)
+            {
+                ViewData["TooYoung"] = $"{userName} is still in high school.";
+            }
+            if (age >= 25)
+            {
+                ViewData["CanRent"] = $"{userName} can drive.";
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
